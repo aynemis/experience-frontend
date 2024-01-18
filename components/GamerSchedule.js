a399305dddf817c478e2cc1acda01b368d5e7198
Yasmine <yasmine.pvt@gmail.com>
2024-01-18 13:12:44 +0100

    css

--- components/GamerSchedule.js
+++ components/GamerSchedule.js
@@ -82,7 +82,6 @@ function GamerSchedule(props) {
         </div>
 
         <div className=" flex flex-col items-stretch align-center">
-          <p className="text-lg mb-7 ">My Schedule</p>
           <div className="flex flex-col justify-center w-full items-center">
             <div className="bg-base-100 rounded-xl">
               <DayPicker
